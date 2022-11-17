2f5059dde82d1dd2d4d59279cdc2e4eaed8c26d7
Ted Steen <ted.steen@gmail.com>
2022-11-17 17:01:17 +0200

    created svelte project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,2 @@
-<script>
-    export let data;
-    const { fetchResp } = data
-</script>
-
-Hello world!!
-{fetchResp.country.name}
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
